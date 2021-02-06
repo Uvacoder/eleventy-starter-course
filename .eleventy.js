d8289ceb2ca895febc19b7b4b4ab71de0baeb63c
ordyboii <jake.ord345@gmail.com>
2021-02-06 19:08:28 +0000

    Added in production ready optimisations

--- .eleventy.js
+++ .eleventy.js
@@ -2,9 +2,13 @@ const sortByDisplayOrder = require("./src/utils/sort-by-display-order.js");
 const dateFilter = require("./src/filters/date-filter.js");
 const w3DateFilter = require("./src/filters/w3-date-filter.js");
 const rssPlugin = require("@11ty/eleventy-plugin-rss");
+const htmlMinTransform = require("./src/transforms/html-min-transform.js");
+const isProduction = process.env.NODE_ENV === "production";
 
 module.exports = (config) => {
-  config.addPassthroughCopy("./src/images/");
+  if (isProduction) {
+    config.addTransform("htmlmin", htmlMinTransform);
+  }
 
   config.addFilter("dateFilter", dateFilter);
   config.addFilter("w3DateFilter", w3DateFilter);
